46f460c3624f31a9f44ae9aa801549f97939b2aa
weaver312 <43344069+weaver312@users.noreply.github.com>
2021-09-26 16:36:24 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,2 +1,7 @@
 npm install
+ls ./node_modules/webpack-cli/bin/
+ls ./node_modules/webpack-cli/bin/webpack
+npm install webpack-cli
+ls ./node_modules/webpack-cli/bin/
+ls ./node_modules/webpack-cli/bin/webpack
 npm run build
